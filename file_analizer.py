0b90874e650482a40407a6ea4d12d4b4f370372d
wszoltysek <szoltysek.w@gmail.com>
2020-04-15 14:06:16 +0200

    gitigrone update

--- file_analizer.py
+++ file_analizer.py
@@ -1,68 +1,93 @@
+import argparse
+import sys
+
 done = False
 while not done:
 
-    print()
-    print("WELCOME!")
-    print("This program analyzes the data entered by the user")
-    print()
-    pony = input("Enter your data: ")
+    class FileAnalizer:
 
-    dots_count = pony.count(".")
-    coma_count = pony.count(",")
-    sentence_count = len(pony.split(". "))
+        def __init__(self, filename):
+            self.filename = filename
+            self.all_char_counter = 0
+            self.sentence_counter = 0
+            self.upper_counter = 0
+            self.lower_counter = 0
+            self.dots_counter = 0
+            self.comas_counter = 0
+            self.digits_counter = 0
+            self.spaces_counter = 0
+            self.special_counter = 0
+            self.special_list = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "-", "+", "=", "_", ":", ";"]
+            self.other_counter = 0
+
+        def process_file(self):
+            with open(self.filename, "r") as input_file:
+                txt_file = input_file.read()
+                self.all_char_counter = len(txt_file)
+                self.sentence_counter = len(txt_file.split(". "))
+
+                for char in txt_file:
+                    if char.isupper():
+                        self.upper_counter += 1
+                    elif char.isupper():
+                        self.upper_counter += 1
+                    elif char.islower():
+                        self.lower_counter += 1
+                    elif char == ".":
+                        self.dots_counter += 1
+                    elif char == ",":
+                        self.comas_counter += 1
+                    elif char.isnumeric():
+                        self.digits_counter += 1
+                    elif char.isspace():
+                        self.spaces_counter += 1
+                    elif char in self.special_list:
+                        self.special_counter += 1
+                    else:
+                        self.other_counter += 1
 
 
-    def upper_letters_counter(pony):
-        upperletters = 0
-        for letter in pony:
-            if letter.isupper():
-                upperletters += 1
-        return upperletters
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=str, action="store")
 
-
-    def lower_letters_counter(pony):
-        lowerletters = 0
-        for letter in pony:
-            if letter.islower():
-                lowerletters += 1
-        return lowerletters
-
-
-    def numbers_counter(pony):
-        digits = 0
-        for mark in pony:
-            if mark.isdigit():
-                digits += 1
-        return digits
-
-
-    def special_counter(pony):
-        special_list = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "=", "_"]
-        counter = 0
-        for special in pony:
-            if special in special_list:
-                counter += 1
-        return counter
-
-
-    if len(pony) == 0:
+    if len(sys.argv) < 2:
         print()
-        print("Wiersz nie może być pusty.")
+        print("WELCOME TO TEXT FILE ANALIZER")
+        print("This program analyzes the data from users txt file.")
+        print("To run a program write: \"python3 file_analizer.py your_txt_file.txt\" ")
         print()
+        sys.exit()
+
     else:
-        print()
-        print(f"Długość wiersza to {len(pony)} znaków.")
-        print(f"Wiersz ma {sentence_count} zdania.")
-        print(f"Wiersz ma {upper_letters_counter(pony)} dużych liter.")
-        print(f"Wiersz ma {lower_letters_counter(pony)} małych liter.")
-        print(f"Wiersz ma {dots_count} kropki.")
-        print(f"Wiersz ma {coma_count} przecinków.")
-        print(f"Wiersz ma {numbers_counter(pony)} liczb.")
-        print(f"Wiersz ma {special_counter(pony)} znaków specjalnych.")
-        print()
+        options = parser.parse_args()
+        file = FileAnalizer(options.filename)
+        file.process_file()
 
-    if "yes" != input("Do you want to start over? [Write \"yes\" or \"no\"] "):
+        if file.all_char_counter == 0:
+            print()
+            print("Your text file is empty.")
+            print()
+        else:
+            print(f"Your text file have {file.all_char_counter} characters.")
+            print()
+            print("There are:")
+            print(f"{file.sentence_counter} sentences,")
+            print(f"{file.upper_counter} upper letters,")
+            print(f"{file.lower_counter} lower letters,")
+            print(f"{file.dots_counter} dots,")
+            print(f"{file.comas_counter} comas,")
+            print(f"{file.spaces_counter} white spaces,")
+            print(f"{file.digits_counter} digits,")
+            print(f"{file.special_counter} special characters like: !, ?, #, %, * etc.")
+            print(f"{file.other_counter} others.")
+            print()
+
+    if "yes" == input("Do you want to start over? [Write \"yes\" or \"no\"] "):
+        print()
+        print("WELCOME AGAIN!")
+        print("To run a program write: \"python3 file_analizer.py your_txt_file.txt\" ")
+        print()
+        sys.exit()
+    else:
         done = True
-
-    # TODO: If-owanie printami w zależności od ilości.
-    # TODO: Wszystko po angielsku
+        sys.exit()
