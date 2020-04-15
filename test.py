68dff132c9822075dcc8a4225ef224ada10d8a87
wszoltysek <szoltysek.w@gmail.com>
2020-04-15 12:24:57 +0200

    program counters update

--- test.py
+++ test.py
@@ -1,25 +1,41 @@
 import argparse
 import sys
 
-
 class FileAnalizer:
 
     def __init__(self, filename):
         self.filename = filename
-        self.counter = 0
+        self.file_counter = 0
+        self.upper_counter = 0
+        self.lower_counter = 0
         self.dots_counter = 0
+        self.comas_counter = 0
+        self.digits_counter = 0
+        self.special_list = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "-", "+", "=", "_", ":", ";"]
+        self.special_counter = 0
+        self.sentence_counter = 0
 
     def process_file(self):
         with open(self.filename, "r") as input_file:
+
             while True:
                 char = input_file.read(1)
+                self.file_counter += len(char)
+
                 if not char:
                     break
-
-                if char.isupper():
-                    self.counter += 1
+                elif char.isupper():
+                    self.upper_counter += 1
+                elif char.islower():
+                    self.lower_counter += 1
                 elif char == ".":
                     self.dots_counter += 1
+                elif char == ",":
+                    self.comas_counter += 1
+                elif char in self.special_list:
+                    self.special_counter += 1
+                elif char == ". ":
+                    self.sentence_counter += 1
 
 
 parser = argparse.ArgumentParser()
@@ -27,9 +43,9 @@ parser.add_argument("filename", type=str, action="store")
 
 if len(sys.argv) < 2:
     print()
-    print("WELCOME TO FILE ANALIZER")
+    print("WELCOME TO TEXT FILE ANALIZER")
     print("This program analyzes the data from users txt file.")
-    print("To run a program ...")
+    print("To run a program write: \"python3 file_analizer.py your_txt_file.txt\" ")
     print()
     sys.exit()
 
@@ -37,5 +53,18 @@ else:
     options = parser.parse_args()
     file = FileAnalizer(options.filename)
     file.process_file()
-    print(file.counter)
-    print(file.dots_counter)
+
+    if file.file_counter == 0:
+        print()
+        print("Your text file is empty.")
+        print()
+    else:
+        print(f"Your text file have {file.file_counter} characters.")
+        print()
+        print("There are:")
+        print(f"{file.upper_counter} upper letters,")
+        print(f"{file.lower_counter} lower letters,")
+        print(f"{file.dots_counter} dots,")
+        print(f"{file.comas_counter} comas,")
+        print(f"{file.special_counter} special characters like: !, ?, #, %, * etc.")
+        # print("zdania", file.sentence_counter)
