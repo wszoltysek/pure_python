667333f618a2653b1174a1f22d789041614424cc
wszoltysek <szoltysek.w@gmail.com>
2020-04-27 13:12:34 +0200

    sort vs sorted

--- some_practice.py
+++ some_practice.py
@@ -15,14 +15,32 @@
 
 # ARGS / KWARGS:
 
-def test(*args, **kwargs):
-    for arg in args:
-        print(arg)
+# def test_args(*args):
+#     for arg in args:
+#         print(arg)
+#
+#
+# def test_kwargs(**kwargs):
+#     for key in kwargs:
+#         print(f"{key} = {kwargs[key]}")
+#
+#
+# art = "Alala"
+# ama = "lalalla mama dwa"
+# lama = [1, 2, 3, 4, "debug"]
+# diction = {"ala": "pies", "magda": "kot", "1": "number", 1: "liczba", "lista": [1, "cos", 3]}
+#
+# print(test_args(art, ama, lama, diction))
+# print(test_kwargs(nazwa="Han Solo", opis="smuggler"))
 
 
-art = "Alala"
-ama = "lalalla mama dwa"
-lama = [1, 2, 3, 4, "debug"]
-diction = {"ala": "pies", "magda": "kot", "1": "number", 1: "liczba", "lista": [1, "cos", 3]}
+# SORTOWANIE:
 
-print(test(art, ama, lama, diction))
+int_list_one = [1, 3, 7, 2, 42]
+int_list_two = [1, 0, 17, 34, 22, 3, 7, 2, 42]
+
+int_list_one.sort()
+sorted_list = sorted(int_list_two)
+
+print(int_list_one)
+print(sorted_list)
