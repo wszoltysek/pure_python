6b999160c78f88f8783e9a24f186b721a72be317
wszoltysek <szoltysek.w@gmail.com>
2020-04-27 11:33:51 +0200

    args, kwargs

--- some_practice.py
+++ some_practice.py
@@ -1,13 +1,28 @@
 # GENERATORY:
 
 
-def veoy(value):
-    sam_list = ["a", "b", "e", "y"]
-    for sam in value:
-        if sam in sam_list:
-            yield sam
+# def veoy(value):
+#     sam_list = ["a", "b", "e", "y"]
+#     for sam in value:
+#         if sam in sam_list:
+#             yield sam
+#
+#
+# smt = "ala i ewelina ma kota"
+#
+# print([sam for sam in veoy(smt)])
 
 
-smt = "ala i ewelina ma kota"
+# ARGS / KWARGS:
 
-print([sam for sam in veoy(smt)])
+def test(*args, **kwargs):
+    for arg in args:
+        print(arg)
+
+
+art = "Alala"
+ama = "lalalla mama dwa"
+lama = [1, 2, 3, 4, "debug"]
+diction = {"ala": "pies", "magda": "kot", "1": "number", 1: "liczba", "lista": [1, "cos", 3]}
+
+print(test(art, ama, lama, diction))
