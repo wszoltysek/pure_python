e3c88fb66cab1f58c5f3052cbee39f53a157c359
wszoltysek <szoltysek.w@gmail.com>
2020-05-03 14:58:51 +0200

    added welcome message for task organizer

--- app_msgs.py
+++ app_msgs.py
@@ -28,3 +28,19 @@ end_msg = """
 
 #########################################################################
 """
+
+task_intro = """
+#####################################################################################################################
+
+                                        WELCOME TO THE TASK ORGANIZER!
+
+                                    Enter command and data like in examples:
+                
+add --name "TASK NAME" --deadline "TASK DEADLINE" --description "TASK DESCRIPTION"
+update --name "TASK NEW NAME" --deadline "TASK NEW DEADLINE" --description "TASK NEW DESCRIPTION" --hash "TASK HASH"
+remove --hash "TASK HASH"
+list --all
+list --today
+
+#####################################################################################################################
+"""
