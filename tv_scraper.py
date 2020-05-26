f41825719759bcc3c2a1ed9ea6e28f131d6c9f1e
wszoltysek <szoltysek.w@gmail.com>
2020-05-26 17:19:30 +0200

    code and output reformat

--- tv_scraper.py
+++ tv_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import datetime
 
 
 class TVScraper:
@@ -20,30 +21,31 @@ class TVScraper:
 
         # Shows time:
         hours = TVScraper.page_parser().find_all("em")
-        hours_list = [hour.text for hour in hours]
+        hours_list = [hour.text for hour in hours if len(hour.text) > 1]
 
         # Shows details:
         shows_details = TVScraper.page_parser().find_all("div", {"class": "detail"})
-        shows_name_list = [name.text for a in shows_details for name in a.find_all("a") if len(name.text) > 1]
-        shows_genre_list = [desc.text for p in shows_details for desc in p.find_all("p", {"class": "genre"})]
+        shows_name_list = [name.text for a in shows_details
+                           for name in a.find_all("a") if len(name.text) > 1]
+        shows_genre_list = [desc.text for p in shows_details
+                            for desc in p.find_all("p", {"class": "genre"}) if len(desc.text) > 1]
 
+        fmt = "{:<10}{:<35}{}"
+
+        print(f"STACJA: {station_name}")
+        print(f"Data programu: {datetime.date.today()}\n")
+        print(fmt.format("GODZINA", "PROGRAM", "OPIS"))
         for h, n, g in zip(hours_list, shows_name_list, shows_genre_list):
-            print(f"Godzina: {h}, Program: {n}, Opis: {g}")
+            print(fmt.format(h, n, g))
 
 
 if __name__ == "__main__":
     TVScraper.page_content_scraper()
 
 
-
 """
 ZAMYSŁ: 
-- Print Nazwa stacji: <nazwa>
-- Print Data programu: Date.today
-- Print (dane zapisane)
 - lista jako kolumna w sql lite - zapis !
-- trzecia lista to opis
 - dopisz kolumne date z date.today przy zapisie
 - docstring
-
 """
