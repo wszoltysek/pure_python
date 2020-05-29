5400e8230bd06e5c54a54f25a769cf5c14e85530
wszoltysek <szoltysek.w@gmail.com>
2020-05-29 13:18:49 +0200

    reformat to context manager

--- blog_scraper.py
+++ blog_scraper.py
@@ -22,19 +22,17 @@ class BlogScraper:
         content = page.find("div", {"class": "wpb_column vc_column_container vc_col-sm-8"})
         articles = content.find("div", {"class": "row"}).find_all("article")
 
-        csv_file = open('blog_content.csv', 'w')
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["title", "date", "summary", "link"])
+        with open("blog_content.csv", "w") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["title", "date", "summary", "link"])
 
-        for single_art in articles:
-            title = single_art.h2.text
-            date = single_art.li["datetime"]
-            summary = single_art.p.text
-            link = single_art.a["href"]
+            for single_art in articles:
+                title = single_art.h2.text
+                date = single_art.li["datetime"]
+                summary = single_art.p.text
+                link = single_art.a["href"]
 
-            csv_writer.writerow([title, date, summary, link])
-
-        csv_file.close()
+                csv_writer.writerow([title, date, summary, link])
 
 
 if __name__ == "__main__":
