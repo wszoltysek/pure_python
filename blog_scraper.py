7297fb3d96d6bd33ff34d4fa054240aecf4646ae
wszoltysek <szoltysek.w@gmail.com>
2020-05-29 13:13:06 +0200

    saving content to the csv file

--- blog_scraper.py
+++ blog_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 class BlogScraper:
@@ -21,11 +22,19 @@ class BlogScraper:
         content = page.find("div", {"class": "wpb_column vc_column_container vc_col-sm-8"})
         articles = content.find("div", {"class": "row"}).find_all("article")
 
+        csv_file = open('blog_content.csv', 'w')
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["title", "date", "summary", "link"])
+
         for single_art in articles:
-            titles = single_art.h2.text
+            title = single_art.h2.text
             date = single_art.li["datetime"]
             summary = single_art.p.text
             link = single_art.a["href"]
+
+            csv_writer.writerow([title, date, summary, link])
+
+        csv_file.close()
 
 
 if __name__ == "__main__":
