2c6fde1d7be0ef85a3d295ab2b7fc60c857f9f6e
wszoltysek <szoltysek.w@gmail.com>
2020-05-30 12:05:01 +0200

    urls pattern

--- regex_practice/regex_practice.py
+++ regex_practice/regex_practice.py
@@ -3,6 +3,7 @@ import re
 with open("data.txt", "r") as data:
     data_file = data.read()
 
+
 # full name:
 full_name_pattern = re.compile(r"[A-Z]{1}[a-z]{3,10}\s[A-Z]{1}[a-z]{3,10}")
 fn_matches = full_name_pattern.finditer(data_file)
@@ -10,12 +11,14 @@ fn_matches = full_name_pattern.finditer(data_file)
 # for match in fn_matches:
 #     print(match.group())
 
+
 # phone number:
 phone_pattern = re.compile(r"\d{3}[*-.]+\d{3}[*-.]+\d{4}")
 ph_matches = phone_pattern.finditer(data_file)
 
 # for match in ph_matches:
 #     print(match.group())
+
 
 # adresses:
 addr_pattern = re.compile(
@@ -26,9 +29,21 @@ addr_matches = addr_pattern.finditer(data_file)
 # for match in addr_matches:
 #     print(match.group())
 
+
 # emails:
 email_pattern = re.compile(r"([a-z]+)([.]*)([a-z]*)@([a-z]+)([\.a-z]+)([\.a-z]*)")
 email_matches = email_pattern.finditer(data_file)
 
-emails_list = [match.group() for match in email_matches]
-print(emails_list)
+# emails_list = [match.group() for match in email_matches]
+# print(emails_list)
+
+
+# urls:
+with open("urls.txt", "r") as file:
+    url_file = file.read()
+
+url_pattern = re.compile(r"https?://(www\.)?\w+\.\w+")
+url_matches = url_pattern.finditer(url_file)
+
+for match in url_matches:
+    print(match.group())
