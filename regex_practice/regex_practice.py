4bed2d829698ed180f9434dd3850bf8b3fe95fc3
wszoltysek <szoltysek.w@gmail.com>
2020-05-30 10:57:18 +0200

    phone pattern

--- regex_practice/regex_practice.py
+++ regex_practice/regex_practice.py
@@ -7,5 +7,12 @@ with open("data.txt", "r") as data:
 full_name_pattern = re.compile(r"[A-Z]{1}[a-z]{3,10}\s[A-Z]{1}[a-z]{3,10}")
 fn_matches = full_name_pattern.finditer(data_file)
 
-for match in fn_matches:
+# for match in fn_matches:
+#     print(match.group())
+
+# phone number:
+phone_pattern = re.compile(r"\d{3}[*-.]+\d{3}[*-.]+\d{4}")
+ph_matches = phone_pattern.finditer(data_file)
+
+for match in ph_matches:
     print(match.group())
