9e6a241e22e18d1d13a94e66d31d81ec85de3394
wszoltysek <szoltysek.w@gmail.com>
2020-06-05 14:36:03 +0200

    calculator update

--- calculator.py
+++ calculator.py
@@ -1,49 +1,67 @@
 print("## WELCOME TO CALCULATOR ##")
 intro = """
-Insert calculator choice:
+OPTIONS:
 1 - add
 2 - subtract
 3 - multiply
 4 - divide
 """
 print(intro)
-choice = input("Your choice: ")
-x = int(input("First number: "))
-y = int(input("Second number: "))
+choice = input("Type option number: ")
 
-if choice == "1":
+try:
+    if choice == "1":
+        x = int(input("First number: "))
+        y = int(input("Second number: "))
 
-    def add(x, y):
-        """Add Function"""
-        return x + y
 
-    print(f"\nResult of add {x} + {y} is {add(x, y)}.")
+        def add(x, y):
+            """Add Function"""
+            return x + y
 
-elif choice == "2":
 
-    def subtract(x, y):
-        """Subtract Function"""
-        return x - y
+        print(f"\nResult of add {x} + {y} is {add(x, y)}.")
 
-    print(f"\nResult of subtract {x} - {y} is {subtract(x, y)}.")
+    elif choice == "2":
+        x = int(input("First number: "))
+        y = int(input("Second number: "))
 
-elif choice == "3":
 
-    def multiply(x, y):
-        """Multiply Function"""
-        return x * y
+        def subtract(x, y):
+            """Subtract Function"""
+            return x - y
 
-    print(f"\nResult of multiply {x} * {y} is {multiply(x, y)}.")
 
-elif choice == "4":
+        print(f"\nResult of subtract {x} - {y} is {subtract(x, y)}.")
 
-    def divide(x, y):
-        """Divide Function"""
-        if x == 0 or y == 0:
-            raise ValueError("Can not divide by zero!")
-        return x / y
+    elif choice == "3":
+        x = int(input("First number: "))
+        y = int(input("Second number: "))
 
-    print(f"\nResult of divide {x} / {y} is {divide(x, y)}.")
 
-else:
-    print("Invalid command. Try again.")
+        def multiply(x, y):
+            """Multiply Function"""
+            return x * y
+
+
+        print(f"\nResult of multiply {x} * {y} is {multiply(x, y)}.")
+
+    elif choice == "4":
+        x = int(input("First number: "))
+        y = int(input("Second number: "))
+
+
+        def divide(x, y):
+            """Divide Function"""
+            if x == 0 or y == 0:
+                raise ValueError("Can not divide by zero!")
+            return x / y
+
+
+        print(f"\nResult of divide {x} / {y} is {divide(x, y)}.")
+
+    else:
+        print("Invalid command. Try again.")
+
+except ValueError:
+    print("Wrong number. Must be int.")
