81c67ea65b9f2436014873fddd11d55b3c15c1c5
wszoltysek <szoltysek.w@gmail.com>
2020-06-16 11:16:14 +0200

    calculator update. loops working

--- calculator.py
+++ calculator.py
@@ -1,67 +1,84 @@
+import sys
+
 print("## WELCOME TO CALCULATOR ##")
-intro = """
-OPTIONS:
-1 - add
-2 - subtract
-3 - multiply
-4 - divide
-"""
-print(intro)
-choice = input("Type option number: ")
 
-try:
-    if choice == "1":
-        x = int(input("First number: "))
-        y = int(input("Second number: "))
+finish = False
+while not finish:
 
+    intro = """
+    SELECT OPTIONS:
+    1 - add
+    2 - subtract
+    3 - multiply
+    4 - divide
+    """
+    print(intro)
+    choice = input("Type option number: ")
+    while choice not in ["1", "2", "3", "4"]:
+        print("Choice incorrect. Try again.")
+        choice = input("Type option number: ")
 
-        def add(x, y):
-            """Add Function"""
-            return x + y
-
-
-        print(f"\nResult of add {x} + {y} is {add(x, y)}.")
-
-    elif choice == "2":
-        x = int(input("First number: "))
-        y = int(input("Second number: "))
+    try:
+        if choice == "1":
+            print("# Addition #\n")
+            x = float(input("First number: "))
+            y = float(input("Second number: "))
 
 
-        def subtract(x, y):
-            """Subtract Function"""
-            return x - y
+            def add(x, y):
+                """Add Function"""
+                return x + y
 
 
-        print(f"\nResult of subtract {x} - {y} is {subtract(x, y)}.")
+            print(f"\nResult of add {x} + {y} is {add(x, y)}.")
 
-    elif choice == "3":
-        x = int(input("First number: "))
-        y = int(input("Second number: "))
-
-
-        def multiply(x, y):
-            """Multiply Function"""
-            return x * y
+        elif choice == "2":
+            print("Subtraction\n")
+            x = float(input("First number: "))
+            y = float(input("Second number: "))
 
 
-        print(f"\nResult of multiply {x} * {y} is {multiply(x, y)}.")
-
-    elif choice == "4":
-        x = int(input("First number: "))
-        y = int(input("Second number: "))
+            def subtract(x, y):
+                """Subtract Function"""
+                return x - y
 
 
-        def divide(x, y):
-            """Divide Function"""
-            if x == 0 or y == 0:
-                raise ValueError("Can not divide by zero!")
-            return x / y
+            print(f"\nResult of subtract {x} - {y} is {subtract(x, y)}.")
+
+        elif choice == "3":
+            print("Multiplication\n")
+            x = float(input("First number: "))
+            y = float(input("Second number: "))
 
 
-        print(f"\nResult of divide {x} / {y} is {divide(x, y)}.")
+            def multiply(x, y):
+                """Multiply Function"""
+                return x * y
 
+
+            print(f"\nResult of multiply {x} * {y} is {multiply(x, y)}.")
+
+        elif choice == "4":
+            print("Division\n")
+            x = float(input("First number: "))
+            y = float(input("Second number: "))
+
+
+            def divide(x, y):
+                """Divide Function"""
+                if x == 0 or y == 0:
+                    raise ValueError("Can not divide by zero!")
+                return x / y
+
+
+            print(f"\nResult of divide {x} / {y} is {divide(x, y)}.")
+
+    except ValueError:
+        print("Incorrect value. Must be int or float.")
+
+    if "yes" == input("\nDo you want to start over? [Write \"yes\" or type any key to exit.] \n"):
+        print("Thanks for using calculator again!")
     else:
-        print("Invalid command. Try again.")
-
-except ValueError:
-    print("Wrong number. Must be int.")
+        done = True
+        print("Thanks for using calculator. GoodBye!")
+        sys.exit()
