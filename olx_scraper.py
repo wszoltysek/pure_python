79033e446882dd53804aaf8c89f8a1e34eb00cc6
wszoltysek <szoltysek.w@gmail.com>
2020-09-08 11:49:31 +0200

    olx scraper with selenium - in progress

--- olx_scraper.py
+++ olx_scraper.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -16,21 +19,23 @@ if cookies_accept is not None:
 # Search box
 search_box = driver.find_element_by_id("headerSearch")
 search_box.send_keys("Yamaha WR")
-search_box.send_keys(Keys.ENTER)
+search_button = driver.find_element_by_id("submit-searchmain").click()
 
 # Wait 5 sec to load page.
 time.sleep(5)
 
-bikes = driver.find_elements_by_class_name("wrap")
+# while True:
+#     try:
+#         bikes = driver.find_elements_by_class_name("wrap")
+#         # Bike names:
+#         bike_name_list = [bike.find_element_by_tag_name("strong").text for bike in bikes]
+#         # Bike links:
+#         bike_links = [bike.find_element_by_tag_name("a").get_attribute("href") for bike in bikes]
+#         # Bike prices:
+#         bike_prices = [bike.find_element_by_class_name("price").text for bike in bikes]
 
-# Bike names:
-bike_name_list = [bike.find_element_by_tag_name("strong").text for bike in bikes]
 
-# Bike links:
-bike_links = [bike.find_element_by_tag_name("a").get_attribute("href") for bike in bikes]
-
-# Bike prices:
-bike_prices = [bike.find_element_by_class_name("price").text for bike in bikes]
+# driver.find_element_by_class_name("wrapper").find_element_by_class_name("pager rel clr")
 
 
 # driver.quit()
