ee8499b763b0cd57a6ed25839cce6657a71bcc37
wszoltysek <szoltysek.w@gmail.com>
2020-09-10 15:50:37 +0200

    code reformat

--- olx_scraper_v2.py
+++ olx_scraper_v2.py
@@ -9,16 +9,16 @@ class Scrapper:
         self.query_url = 'https://www.olx.pl/oferty/q-{}/'
         self.page_count = 0
 
-    def get_all_by_asyncs(self):
-        urls = [self.query_url.format(self.query) + '?page={}'.format(i) for i in range(self.page_count)]
-        rs = (grequests.get(u) for u in urls)
-        self.scrapped_pages = grequests.map(rs)
-
     def get_page_count(self):
         r = grequests.map([grequests.get(self.query_url.format(self.query))])
         soup = BeautifulSoup(r[0].content, 'lxml')
         counter_last = soup.find_all('a', {"class": "block br3 brc8 large tdnone lheight24"})
         self.page_count = int(counter_last[-1].text)
+
+    def get_all_by_asyncs(self):
+        urls = [self.query_url.format(self.query) + '?page={}'.format(i) for i in range(self.page_count)]
+        rs = (grequests.get(u) for u in urls)
+        self.scrapped_pages = grequests.map(rs)
 
     def scrap_offers_from_page(self):
         for page in self.scrapped_pages:
